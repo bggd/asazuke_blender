f1b0b4a5a8fb093f75d47d757fdfee4c2808d639
bggd <gameovera@live.com>
2024-01-29 21:17:14 +0900

    export Collection

--- asazuke_blender/export_asazuke.py
+++ asazuke_blender/export_asazuke.py
@@ -1,6 +1,38 @@
 import bpy
+import pathlib
 from asazuke import asazuke_pb2
+
+def gen_collection_tree(collection, c: asazuke_pb2.Collection):
+    for i in c.children:
+        child = collection.children.add()
+        child.name = i.name
+
+        if i.library:
+            library = i.library
+            p = pathlib.Path(library.filepath[2:])
+            collection.library_file_path = p.as_posix()
+
+        gen_collection_tree(child, i)
+
+
 
 def export_asazuke():
     a = asazuke_pb2.Asazuke()
+
+    for s in bpy.data.scenes:
+        scene = a.scenes.add()
+        scene.name = s.name
+
+        collection = asazuke_pb2.Collection()
+
+        collection.name = s.collection.name
+        if s.collection.library:
+            library = s.collection.library
+            p = pathlib.Path(library.filepath[2:])
+            collection.library_file_path = p.as_posix()
+
+        gen_collection_tree(collection, s.collection)
+
+        scene.collection.CopyFrom(collection)
+
     return a
--- tests/test_default_scene.py
+++ tests/test_default_scene.py
@@ -4,10 +4,22 @@ from asazuke import asazuke_pb2
 import asazuke_blender
 
 class TestDefaultScene(unittest.TestCase):
+
     def test_default_scene(self):
         a = asazuke_blender.export_asazuke()
 
         self.assertTrue(isinstance(a, asazuke_pb2.Asazuke))
+
+        self.assertEqual(len(a.scenes), 1)
+        self.assertEqual(a.scenes[0].name, "Scene")
+
+        collection = a.scenes[0].collection
+        self.assertEqual(collection.name, "Scene Collection")
+        self.assertEqual(len(collection.children), 1)
+
+        child_collection = collection.children[0]
+        self.assertEqual(child_collection.name, "Collection")
+        self.assertEqual(child_collection.library_file_path, "")
 
 if __name__ == "__main__":
     import sys
